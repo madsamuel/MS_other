a965db01009f951d6366c7c7b87cdccd2fc5bfa8
madsamuel <madsamuel@gmail.com>
2025-03-07 01:27:21 -0800

    3-7 fixes

--- perf_tool_csharp/custom_notifications/SmartConnectAdvisorAI/obj/Debug/net9.0-windows10.0.19041.0/SmartConnectAdvisorAI.AssemblyInfo.cs
+++ perf_tool_csharp/custom_notifications/SmartConnectAdvisorAI/obj/Debug/net9.0-windows10.0.19041.0/SmartConnectAdvisorAI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartConnectAdvisorAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0f578f231925ee6a916d3c05d6c3f335f92ab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afdffab75f777828686049ae96658db0cf6cfa91")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartConnectAdvisorAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartConnectAdvisorAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
