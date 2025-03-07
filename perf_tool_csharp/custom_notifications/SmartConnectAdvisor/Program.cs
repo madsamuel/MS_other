3ff6de39c55599263d31067e0458b6c6d1184ff2
madsamuel <madsamuel@gmail.com>
2025-03-06 21:00:38 -0800

    3-6 functional notification

--- perf_tool_csharp/custom_notifications/SmartConnectAdvisor/Program.cs
+++ perf_tool_csharp/custom_notifications/SmartConnectAdvisor/Program.cs
@@ -4,26 +4,26 @@ using Windows.UI.Notifications;             // For the actual 'ToastNotification
 
 // See https://aka.ms/new-console-template for more information
 // We'll specify an app ID so Windows knows who is sending the toast.
-    string appId = "MyCompany.MyToastApp";
-    
-    // Build the toast content (title, message, two buttons, etc.)
-    var toastContent = new ToastContentBuilder()
-        .AddText("Network Alert")
-        .AddText("Your connection is degrading, please take action soon.")
-        .AddButton(new ToastButton()
-            .SetContent("Fix Now")               // What the button displays
-            .AddArgument("action", "fixNow"))    // Argument for activation (if using COM/advanced)
-        .AddButton(new ToastButton()
-            .SetContent("Ignore")                // Second button
-            .AddArgument("action", "ignore"))
-        .GetToastContent();
+string appId = "MyCompany.MyToastApp";
 
-    // Create the actual toast notification from the XML content
-    var toast = new ToastNotification(toastContent.GetXml());
+// Build the toast content (title, message, two buttons, etc.)
+var toastContent = new ToastContentBuilder()
+    .AddText("Network Alert")
+    .AddText("Your connection is degrading, please take action soon.")
+    .AddButton(new ToastButton()
+        .SetContent("Fix Now")               // What the button displays
+        .AddArgument("action", "fixNow"))    // Argument for activation (if using COM/advanced)
+    .AddButton(new ToastButton()
+        .SetContent("Ignore")                // Second button
+        .AddArgument("action", "ignore"))
+    .GetToastContent();
 
-    // Use the CommunityToolkit helper to create and show the toast
-    ToastNotificationManagerCompat.CreateToastNotifier();
+// Create the actual toast notification from the XML content
+var toast = new ToastNotification(toastContent.GetXml());
 
-    // Keep console open so we can observe the result
-    Console.WriteLine("Toast with two buttons has been sent. Press any key to exit.");
-    Console.ReadKey();
+// Use the CommunityToolkit helper to create and show the toast
+ToastNotificationManagerCompat.CreateToastNotifier().Show(toast);
+
+// Keep console open so we can observe the result
+Console.WriteLine("Toast with two buttons has been sent. Press any key to exit.");
+Console.ReadKey();
