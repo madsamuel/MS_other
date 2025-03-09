fcc291cce4797ffa53b142d077fd1e557028921a
madsamuel <madsamuel@gmail.com>
2025-03-09 01:11:05 -0800

    3-9 ksp_kotlin_room_working
    This is a template that shows how to get Kotlin Symbol Processing (KSP) and Kotlin to work together with latest version as of Q3 2025.

---
+++ code/android/ksp_kotlin_room_working/app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    // KSP for annotation processing (Room, Hilt)
+    id("com.google.devtools.ksp")
+
+}
+
+android {
+    namespace = "com.seattledevcamp.projectmanagementguru"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.seattledevcamp.projectmanagementguru"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.kotlinx.coroutines.android) // For coroutines
+    implementation(libs.androidx.navigation.compose) // Navigation Compose
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    // Coroutines, Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx.v261)
+    implementation(libs.kotlinx.coroutines.android)
+    // Navigation Compose
+    implementation(libs.androidx.navigation.compose.v272)
+    // Room (KSP)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler) // this breaks the build figure it out
+    // Hilt (KSP)
+    implementation(libs.hilt.android.v248)
+    ksp(libs.hilt.android.compiler) // this breaks the build figure it out
+    // Hilt Navigation Compose
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Android Core SplashScreen for Android 12+ splash
+    implementation(libs.androidx.core.splashscreen)
+}
