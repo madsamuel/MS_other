e169039eae027c74dde912bd439704cec80dd558
madsamuel <madsamuel@gmail.com>
2025-04-30 00:36:01 -0700

    4/30 fixes
    1) camera location
    2) rotation speed

--- code/fun/3d_cube.py
+++ code/fun/3d_cube.py
@@ -2,9 +2,12 @@ from ursina import *
 
 app = Ursina()
 
+# Set the background color to black
+window.color = color.black
+
 # Function to create a small cube
 def create_small_cube(position, size):
-    return Entity(model='cube', position=position, scale=size, color=color.cyan)
+    return Entity(model='cube', position=position, scale=(size, size, size), color=color.cyan, parent=pivot)
 
 # Function to create a large cube composed of smaller cubes
 def create_large_cube(size, num_cubes):
@@ -17,17 +20,20 @@ def create_large_cube(size, num_cubes):
                             k * small_cube_size - size/2 + small_cube_size/2)
                 create_small_cube(position, small_cube_size)
 
-# Create a large cube composed of smaller cubes
-create_large_cube(size=10, num_cubes=3)
-
 # Create a pivot entity to rotate the large cube
 pivot = Entity()
 
+# Create a large cube composed of smaller cubes
+large_cube_size = 5
+num_cubes = 3
+create_large_cube(size=large_cube_size, num_cubes=num_cubes)
+
 # Function to update the rotation of the large cube
 def update():
-    pivot.rotation_y += 1  # Rotate around the Y-axis
-    pivot.rotation_x += 1  # Rotate around the X-axis
+    pivot.rotation_x += 0.2  # Slowly rotate around the X-axis
 
-EditorCamera()
+# Set the camera position and rotation to fit the cube in the window
+camera.position = (50, 50, 50)
+camera.look_at(pivot)
 
 app.run()
