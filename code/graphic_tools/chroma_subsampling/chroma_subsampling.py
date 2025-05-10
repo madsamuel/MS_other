54e4b536a0a2e103508947bf87f404a932df4fc7
madsamuel <madsamuel@gmail.com>
2025-05-10 00:15:03 -0700

    5/10 add ability to zoom into the images using the mouse scroll

--- code/graphic_tools/chroma_subsampling/chroma_subsampling.py
+++ code/graphic_tools/chroma_subsampling/chroma_subsampling.py
@@ -5,32 +5,29 @@ import numpy as np
 
 def ycbcr_subsample(img, subsample_type):
     if subsample_type == "4:4:4":
-        return img  # No subsampling; return original
+        return img
 
     img_ycbcr = img.convert('YCbCr')
     y, cb, cr = img_ycbcr.split()
-
     y_np = np.array(y)
     cb_np = np.array(cb)
     cr_np = np.array(cr)
-
     h, w = cb_np.shape
 
     def pad_to_even(arr, axis):
-        size = arr.shape[axis]
-        if size % 2 != 0:
+        if arr.shape[axis] % 2 != 0:
             pad_width = ((0, 1), (0, 0)) if axis == 0 else ((0, 0), (0, 1))
             arr = np.pad(arr, pad_width, mode='edge')
         return arr
 
     if subsample_type in ["4:2:2", "4:2:1", "4:1:1"]:
-        cb_np = pad_to_even(cb_np, axis=1)
-        cr_np = pad_to_even(cr_np, axis=1)
+        cb_np = pad_to_even(cb_np, 1)
+        cr_np = pad_to_even(cr_np, 1)
     elif subsample_type == "4:2:0":
-        cb_np = pad_to_even(cb_np, axis=0)
-        cb_np = pad_to_even(cb_np, axis=1)
-        cr_np = pad_to_even(cr_np, axis=0)
-        cr_np = pad_to_even(cr_np, axis=1)
+        cb_np = pad_to_even(cb_np, 0)
+        cb_np = pad_to_even(cb_np, 1)
+        cr_np = pad_to_even(cr_np, 0)
+        cr_np = pad_to_even(cr_np, 1)
 
     if subsample_type == "4:2:2":
         cb_np[:, 1::2] = cb_np[:, ::2]
@@ -49,8 +46,7 @@ def ycbcr_subsample(img, subsample_type):
 
     cb_new = Image.fromarray(cb_np[:h, :w], 'L')
     cr_new = Image.fromarray(cr_np[:h, :w], 'L')
-    img_new = Image.merge("YCbCr", (y, cb_new, cr_new)).convert('RGB')
-    return img_new
+    return Image.merge("YCbCr", (y, cb_new, cr_new)).convert('RGB')
 
 class SubsamplingApp:
     def __init__(self, root):
@@ -60,6 +56,8 @@ class SubsamplingApp:
 
         self.img_original = None
         self.img_processed = None
+        self.zoom_original = 1.0
+        self.zoom_processed = 1.0
 
         self.setup_ui()
 
@@ -80,9 +78,11 @@ class SubsamplingApp:
 
         self.canvas_original = tk.Canvas(self.canvas_frame, bg="black")
         self.canvas_original.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
         self.canvas_processed = tk.Canvas(self.canvas_frame, bg="black")
         self.canvas_processed.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.canvas_original.bind("<MouseWheel>", lambda e: self.zoom(e, 'original'))
+        self.canvas_processed.bind("<MouseWheel>", lambda e: self.zoom(e, 'processed'))
 
         self.root.bind("<Configure>", self.on_resize)
 
@@ -91,6 +91,9 @@ class SubsamplingApp:
         if not path:
             return
         self.img_original = Image.open(path).convert("RGB")
+        self.zoom_original = 1.0
+        self.zoom_processed = 1.0
+        self.img_processed = None
         self.display_images()
 
     def apply_subsampling(self):
@@ -98,27 +101,37 @@ class SubsamplingApp:
             return
         subsample_type = self.subsample_var.get()
         self.img_processed = ycbcr_subsample(self.img_original, subsample_type)
+        self.zoom_processed = 1.0
         self.display_images()
 
     def display_images(self):
-        self._draw_image_on_canvas(self.canvas_original, self.img_original)
+        self._draw_image_on_canvas(self.canvas_original, self.img_original, self.zoom_original)
         if self.img_processed:
-            self._draw_image_on_canvas(self.canvas_processed, self.img_processed)
+            self._draw_image_on_canvas(self.canvas_processed, self.img_processed, self.zoom_processed)
 
-    def _draw_image_on_canvas(self, canvas, img):
+    def _draw_image_on_canvas(self, canvas, img, zoom):
         canvas.delete("all")
         if img is None:
             return
         w = canvas.winfo_width()
         h = canvas.winfo_height()
-        img_resized = img.copy()
-        img_resized.thumbnail((w - 20, h - 20), Image.LANCZOS)
+
+        zoomed_size = (int(img.width * zoom), int(img.height * zoom))
+        img_resized = img.resize(zoomed_size, Image.LANCZOS)
+
         photo = ImageTk.PhotoImage(img_resized)
-        canvas.image = photo  # Keep a reference!
-        img_w, img_h = photo.width(), photo.height()
-        x = (w - img_w) // 2
-        y = (h - img_h) // 2
+        canvas.image = photo  # Prevent garbage collection
+        x = (w - photo.width()) // 2
+        y = (h - photo.height()) // 2
         canvas.create_image(x, y, image=photo, anchor=tk.NW)
+
+    def zoom(self, event, which):
+        delta = 0.1 if event.delta > 0 else -0.1
+        if which == 'original':
+            self.zoom_original = max(0.1, self.zoom_original + delta)
+        elif which == 'processed':
+            self.zoom_processed = max(0.1, self.zoom_processed + delta)
+        self.display_images()
 
     def on_resize(self, event):
         self.display_images()
