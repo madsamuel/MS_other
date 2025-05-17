69dc248db701439614b710bed402e7bb75bfd741
madsamuel <madsamuel@gmail.com>
2025-05-16 19:43:30 -0700

    5/16 rollback

--- code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer/obj/Debug/net9.0-windows/Protocol_Analyzer.AssemblyInfo.cs
+++ code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer/obj/Debug/net9.0-windows/Protocol_Analyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Protocol_Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f73a2449256b8ee3041e7acddd605d814126cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc0578a7186f1e06a13f95362bc932147336cd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Protocol_Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Protocol_Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
