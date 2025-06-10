3a391b8c3d767a02c8ed0539d6661d989e55505f
madsamuel <madsamuel@gmail.com>
2025-06-10 00:28:32 -0700

    6/10 - refactor

--- code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer/obj/Debug/net9.0-windows/Protocol_Analyzer.AssemblyInfo.cs
+++ code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer/obj/Debug/net9.0-windows/Protocol_Analyzer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Protocol_Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eba930b154c0d67128c3caf4de5dc640dbc50df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4278a440180fbbd33f0b2ab92207588e8e4c11d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Protocol_Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Protocol_Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
