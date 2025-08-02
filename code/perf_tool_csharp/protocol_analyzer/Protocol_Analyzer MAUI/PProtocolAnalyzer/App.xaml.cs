63b81c573c650f87fd46adf4da9a8beebc5474d0
madsamuel <madsamuel@gmail.com>
2025-08-01 20:28:31 -0700

    8/1 remove maximize and minimize buttons

--- code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer MAUI/PProtocolAnalyzer/App.xaml.cs
+++ code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer MAUI/PProtocolAnalyzer/App.xaml.cs
@@ -1,10 +1,36 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+#if WINDOWS
+using System.Runtime.InteropServices;
+#endif
 
 namespace PProtocolAnalyzer;
 
 public partial class App : Application
 {
+#if WINDOWS
+	// Win32 API constants and methods for window styling
+	private const int GWL_STYLE = -16;
+	private const int WS_MINIMIZEBOX = 0x20000;
+	private const int WS_MAXIMIZEBOX = 0x10000;
+	
+	[DllImport("user32.dll", SetLastError = true)]
+	private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+	
+	[DllImport("user32.dll", SetLastError = true)]
+	private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+	
+	private static void SetWindowStyle(IntPtr hwnd)
+	{
+		// Get current window style
+		int style = GetWindowLong(hwnd, GWL_STYLE);
+		// Remove minimize and maximize buttons
+		style &= ~(WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+		// Apply new style
+		SetWindowLong(hwnd, GWL_STYLE, style);
+	}
+#endif
+
 	public App()
 	{
 		InitializeComponent();
@@ -24,7 +50,7 @@ public partial class App : Application
 		};
 
 #if WINDOWS
-		// Position window at bottom right of screen
+		// Position window at bottom right of screen and configure window style
 		window.Created += (s, e) =>
 		{
 			var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
@@ -33,6 +59,13 @@ public partial class App : Application
 			
 			window.X = (int)(screenWidth - 700 - 50); // Corrected for 700px width + 50px margin
 			window.Y = (int)(screenHeight - 480 - 50); // Position based on new 480px height
+			
+			// Remove minimize and maximize buttons (Windows only)
+			if (window.Handler?.PlatformView is Microsoft.UI.Xaml.Window winUIWindow)
+			{
+				var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(winUIWindow);
+				SetWindowStyle(hwnd);
+			}
 		};
 #endif
 
