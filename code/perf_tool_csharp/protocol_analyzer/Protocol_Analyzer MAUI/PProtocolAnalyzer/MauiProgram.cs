c5db103684648f55facb9baaa74bec5763b57db4
madsamuel <madsamuel@gmail.com>
2025-11-11 00:01:40 -0800

    11/11 fixes to update legacy parser

--- code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer MAUI/PProtocolAnalyzer/MauiProgram.cs
+++ code/perf_tool_csharp/protocol_analyzer/Protocol_Analyzer MAUI/PProtocolAnalyzer/MauiProgram.cs
@@ -1,36 +1,40 @@
-﻿using Microsoft.Extensions.Logging;
-using PProtocolAnalyzer.Logging;
+﻿namespace PProtocolAnalyzer;
+
 using System;
 using System.IO;
-
-namespace PProtocolAnalyzer;
+using Microsoft.Extensions.Logging;
+using PProtocolAnalyzer.Logging;
+using PProtocolAnalyzer.Services;
 
 public static class MauiProgram
 {
-	public static System.IServiceProvider? Services { get; private set; }
+    public static System.IServiceProvider? Services { get; private set; }
 
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Register services for dependency injection
+        builder.Services.AddSingleton<ISystemInformationService, SystemInformationService>();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-			// Add a simple file logger (writes to LocalAppData\PProtocolAnalyzer\logs\app.log)
-			var logFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PProtocolAnalyzer", "logs");
-			var logFile = Path.Combine(logFolder, "app.log");
-			builder.Logging.AddProvider(new FileLoggerProvider(() => logFile));
+        // Add a simple file logger (writes to LocalAppData\PProtocolAnalyzer\logs\app.log)
+        var logFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PProtocolAnalyzer", "logs");
+        var logFile = Path.Combine(logFolder, "app.log");
+        builder.Logging.AddProvider(new FileLoggerProvider(() => logFile));
 
-	var app = builder.Build();
-	Services = app.Services;
-	return app;
-	}
+        var app = builder.Build();
+        Services = app.Services;
+        return app;
+    }
 }
