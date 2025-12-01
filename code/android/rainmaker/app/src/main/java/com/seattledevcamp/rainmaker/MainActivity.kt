02f1f5313aae16d71f45ee49a8a8bbb61a1361e8
madsamuel <madsamuel@gmail.com>
2025-12-01 00:25:16 -0800

    12/1 cleanup

--- code/android/rainmaker/app/src/main/java/com/seattledevcamp/rainmaker/MainActivity.kt
+++ code/android/rainmaker/app/src/main/java/com/seattledevcamp/rainmaker/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
